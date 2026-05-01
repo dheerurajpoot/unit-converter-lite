35c64d59bf9fd545fe5a5cdda164503d1b075593
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2026-05-01 08:26:46 +0530

    Update constant.ts

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,7 +1,7 @@
 export const SITE_URL = "https://kapve.com";
-export const SITE_NAME = "Kapve";
-export const SITE_TITLE = "Kapve - Unit Converter Lite";
+export const SITE_NAME = "UnitConverterLite";
+export const SITE_TITLE = "Unit Converter Lite - Compute Unit Conversions Instantly.";
 export const SITE_DESCRIPTION =
-	"Free online unit converter. Calculate units, investments, and more instantly. No registration required.";
-export const SITE_AUTHOR = "Kapve";
-export const SITE_EMAIL = "contact@kapve.com";
+	"Free online unit converter. Compute unit conversions, currencies and more instantly. No registration required.";
+export const SITE_AUTHOR = "Deelzo";
+export const SITE_EMAIL = "amitkumarteam90@gmail.com";
